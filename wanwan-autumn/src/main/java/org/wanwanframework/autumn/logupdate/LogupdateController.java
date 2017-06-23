4488546efdfb3f2e191adc2d8446921439776f2d
wanwang2 <1053400545@qq.com>
2017-06-23 20:52:11 +0800

    ...

--- wanwan-autumn/src/main/java/org/wanwanframework/autumn/logupdate/LogupdateController.java
+++ wanwan-autumn/src/main/java/org/wanwanframework/autumn/logupdate/LogupdateController.java
@@ -1,13 +1,54 @@
 package org.wanwanframework.autumn.logupdate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.wanwanframwork.file.FileUtil;
+import org.wanwanframwork.file.Log;
 import org.wanwanframwork.file.core.FileController;
 
-public class LogupdateController extends FileController<String>{
+public class LogupdateController extends FileController<List<String>>{
 
+	protected String method;
+	
+	public LogupdateController() {
+		core = Arrays.asList(FileUtil.readFile("./src/test/java/org/wanwanframework/autumn/LogupdateTestController.java").split("\r\n"));
+	}
+	
 	@Override
 	protected void process() {
-		// TODO Auto-generated method stub
-		super.process();
+		List<String> list = new ArrayList<String>();
+		core.stream().forEach(line -> {
+			
+			String localMethod = processLine(line);
+			if(localMethod != null) {
+				method = localMethod;
+			}
+			if(method != null) {
+				list.add(processErrorLine(line, method));
+			} else {
+				list.add(line);
+			}
+			
+		});
+		Log.log("\r\n" + StringUtils.join(list, "\r\n"));
+	}
+	
+	protected String processLine(String line) {
+		String method = LogupdateTool.getMethod(line);
+		Log.log(method);
+		return method;
+	}
+	
+	protected String processErrorLine(String line, String word) {
+		String errorRegex = "Log.error(\"error\"";
+		if(line.contains(errorRegex)) {
+			line = line.replace(errorRegex, "Log.error(\"@ error\"");
+			line = line.replace("@", word);
+		}
+		return line;
 	}
 	
 	public static void main(String[] args) {
---
+++ wanwan-autumn/src/test/java/org/wanwanframework/autumn/LogupdateTestController.java
@@ -0,0 +1,15 @@
+package org.wanwanframework.autumn;
+
+import org.wanwanframework.autumn.logupdate.LogupdateController;
+import org.wanwanframwork.file.Log;
+
+public class LogupdateTestController extends LogupdateController{
+
+	public void getContent() {
+		try {
+			
+		} catch (Exception e) {
+			Log.error("error", this.getClass());
+		}
+	}
+}
