24cd897b60d66765b97fece734d6ced88aebcd18
wanwang2 <1053400545@qq.com>
2017-06-23 13:49:19 +0800

    。。。

--- wanwan-autumn/src/main/java/org/wanwanframework/autumn/AutumnTool.java
+++ wanwan-autumn/src/main/java/org/wanwanframework/autumn/AutumnTool.java
@@ -1,5 +1,32 @@
 package org.wanwanframework.autumn;
 
+import java.io.File;
+
 public class AutumnTool {
 
+	/**
+	 * 获取Files
+	 * @param dir
+	 * @param type
+	 * @return
+	 */
+	public static File[] readDirByType(File dir, String type) {
+		File[] files = dir.listFiles(file -> {
+			return file.getName().endsWith(type)? true:false;
+		});
+		return files;
+	}
+	
+	public static File[] readDirByXml(File dir) {
+		return readDirByType(dir, ".xml");
+	}
+	
+	/**
+	 * 获取code
+	 * @param name
+	 * @return
+	 */
+	public static String getCode(String name) {
+		return name.substring(0, name.indexOf("."));
+	}
 }
