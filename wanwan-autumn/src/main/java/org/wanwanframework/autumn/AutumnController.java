26cb14228531e2f0cf7fcf5028ff24e691308f87
wanwang2 <1053400545@qq.com>
2017-06-23 15:02:06 +0800

    .

--- wanwan-autumn/src/main/java/org/wanwanframework/autumn/AutumnController.java
+++ wanwan-autumn/src/main/java/org/wanwanframework/autumn/AutumnController.java
@@ -1,5 +1,19 @@
 package org.wanwanframework.autumn;
 
-public class AutumnController {
+import java.util.Map;
 
+import org.wanwanframework.file.map.LineTool;
+import org.wanwanframwork.file.Log;
+import org.wanwanframwork.file.core.FileController;
+
+public class AutumnController extends FileController<Map<String, String>>{
+
+	public AutumnController() {
+		core = LineTool.getLine("./src/main/resources/autumn/core.txt");
+		Log.log(core);
+	}
+	
+	public static void main(String[] args) {
+		AutumnController.call(new AutumnController());
+	}
 }
