26be052d0eb6c308fc4b28fd39b57f3bc4e96ec9
wanwang2 <1053400545@qq.com>
2017-07-26 20:53:41 +0800

    .

--- wanwan-autumn-code/src/main/java/org/wanwanframework/autumn/code/CodeController.java
+++ wanwan-autumn-code/src/main/java/org/wanwanframework/autumn/code/CodeController.java
@@ -1,5 +1,49 @@
 package org.wanwanframework.autumn.code;
 
-public class CodeController {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.wanwanframework.file.config.ConfigController;
+import org.wanwanframwork.file.FileUtil;
+import org.wanwanframwork.file.Log;
+import org.wanwanframwork.file.core.FileController;
+
+public class CodeController extends FileController<List<String>>{
+
+	protected ConfigController configController = new ConfigController("./src/main/resources/context/core.txt");
+	
+	public CodeController() {
+		core = Arrays.asList(FileUtil.readFile("./src/main/resources/template/@ModeDao.java.txt").split("\r\n"));
+	}
+	
+	@Override
+	protected void process() {
+		String[] keys = configController.getCore().get("@Mode").split(",");
+		Arrays.asList(keys).forEach(key ->{
+			processLine(key);
+		});
+	}
+	
+	protected void processLine(String key) {
+		List<String> lineList = new ArrayList<>();
+		core.forEach(string->{
+			string = string.replace("@Mode", key);
+			lineList.add(string);
+		});
+		String lines = StringUtils.join(lineList, "\r\n");
+		Log.log(lines);
+		String path = "./src/main/resources/template/@ModeDao.java.txt".replace("@ModeDao", key);
+		try {
+			FileUtil.createFile(path, lines);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		CodeController.start(CodeController.class);
+	}
 }
